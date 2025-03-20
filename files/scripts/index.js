1ef887d8ae9822bd1919d98b693d8de2cef447b7
Pedro Diaz <pedrojapa2010@hotmail.com>
2025-03-20 17:25:30 -0300

    Feat | Adição sessão clickup

--- files/scripts/index.js
+++ files/scripts/index.js
@@ -12,20 +12,6 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-function openTab(evt, tabName) {
-    let i, content, tabs;
-    content = document.getElementsByClassName("content");
-    for (i = 0; i < content.length; i++) {
-        content[i].style.display = "none";
-    }
-    tabs = document.getElementsByClassName("tab");
-    for (i = 0; i < tabs.length; i++) {
-        tabs[i].className = tabs[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
 
 function openModal(img) {
     let modal = document.getElementById("imageModal");
